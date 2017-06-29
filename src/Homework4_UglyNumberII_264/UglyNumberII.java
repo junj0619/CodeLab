6ef3d2578a12e990069d3c82c2cdcde44962231f
Jun <junj0619@gmail.com>
2017-06-28 23:34:30 -0400

    Create UglyNumberII.java

---
+++ src/Homework4_UglyNumberII_264/UglyNumberII.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int nthUglyNumber(int n) {
+        
+        /*
+         *  (1) 1×2, 2×2, 3×2, 4×2, 5×2, …
+         *  (2) 1×3, 2×3, 3×3, 4×3, 5×3, …
+         *  (3) 1×5, 2×5, 3×5, 4×5, 5×5, … 
+         *
+         *
+        */
+        
+        List<Integer> ugly = new ArrayList<>();
+        ugly.add(1);
+        
+        int p2 = 0;
+        int p3 = 0;
+        int p5 = 0;
+        
+        for (int i = 1; i < n; i++) {
+            int lastNumber = ugly.get(i - 1);
+            
+            while (ugly.get(p2) * 2 <= lastNumber) p2++;
+            while (ugly.get(p3) * 3 <= lastNumber) p3++;
+            while (ugly.get(p5) * 5 <= lastNumber) p5++;
+            
+            ugly.add(Math.min(Math.min(ugly.get(p2) * 2, ugly.get(p3) * 3), ugly.get(p5) * 5));            
+        }
+        
+        return ugly.get(n - 1);
+    }
+}
