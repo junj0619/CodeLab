7ef50951812ddf35638ed87ac90bb35a87d613f2
Jun <junj0619@gmail.com>
2017-06-30 17:01:28 -0400

    Update MeetingRoomII.java

--- src/Class5_MeetingRoomII_253/MeetingRoomII.java
+++ src/Class5_MeetingRoomII_253/MeetingRoomII.java
@@ -7,6 +7,27 @@
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
+
+/**
+* Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+* find the minimum number of conference rooms required.
+*
+* For example,
+* Given [[0, 30],[5, 10],[15, 20]],
+* return 2.
+*
+*
+* Logic : 
+*        1) Flat the Intervals, dump all elements into new list with [isStart] identifier
+*        2) Sort the new list by timeStamp ASC, Corner Case : [1,5,5,7] then sort by [isStart] put 5.end in font of 5.start
+*        3) Loop new list if current element isStart then + 1, else  - 1. Record the max value during scan
+*        4) Return max as MinMeetingRoom
+*
+*        [0, 5, 10, 15, 20, 30]
+*         S  S  E   S   E    E
+*        +1 +1 -1  +1  -1   -1
+*         1  2  1   2   1    0  ==> Max = 2
+**/
 public class MeetingRoom {
     
     class Point {
@@ -38,7 +59,7 @@ public class MeetingRoom {
         for(Point li : list) {
             if(li.isStart == 1) {
                 max = Math.max(++cur, max);
-            }else {
+            } else {
                 cur--;
             }
         }
