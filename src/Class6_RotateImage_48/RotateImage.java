ca9ae00d0be94dcb50412d20422b7ffdbcfef9a9
Jun <junj0619@gmail.com>
2017-07-12 00:58:19 -0400

    Update RotateImage.java

--- src/Class6_RotateImage_48/RotateImage.java
+++ src/Class6_RotateImage_48/RotateImage.java
@@ -24,4 +24,32 @@ public class Solution {
             }
         }
     }
+    
+    
+/*
+*   1, 2, 3     1, 4, 7     7, 4, 1
+*   4, 5, 6  => 2, 5, 8  => 8, 5, 2
+*   7, 8, 9     3, 6, 9     9, 6, 3
+*/
+
+    public void rotateInPlace(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;        
+               
+        for(int i = 0; i < row; i++) {                           // i = 0                     i = 1             i = 2
+            for(int j = i; j < col; j++) {  //!!!! j = i swap from [{0, 0}, {0, 1}, {0, 2}], [{1, 1}, {1, 2}], [{3, 3}]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;                
+            }
+        }      
+        
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col / 2; j++) { //!!!! j < col / 2 swap font end elements until reach mid point
+                int temp = matrix[i][j];                    
+                matrix[i][j] = matrix[i][col - 1 - j];
+                matrix[i][col - 1 - j] = temp;
+            }
+        }
+    }  
 }
