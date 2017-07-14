381812118e08d8402672fa5f0b5ca497fc25ea51
Jun <junj0619@gmail.com>
2017-07-14 17:05:01 -0400

    Create CompleteBinaryTree.java

---
+++ src/Class6_CompleteBinaryTree/CompleteBinaryTree.java
@@ -0,0 +1,38 @@
+/**
+*
+* Check a binary tree is completed or not. 
+* A complete binary tree is a binary tree that every level is completed filled except the deepest level. 
+* In the deepest level, all nodes must be as left as possible. See more definition.
+*
+**/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class ResultType {
+    public boolean isFullTree;
+    public boolean isCompleteTree;
+    int maxDepth;
+    
+    ResultType(boolean isFull, boolean isComplete, int maxDepth) {
+          isFullTree = isFull;
+          isCompleteTree = isComplete;
+          maxDepth = maxDepth;
+    }  
+}
+
+
+public class Solution {
+    public boolean ComplelteBinaryTree(TreeNode root) {
+
+    }
+}
