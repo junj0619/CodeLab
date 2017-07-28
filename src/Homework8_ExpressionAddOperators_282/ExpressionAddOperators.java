15739c906c3902fa4ffb3459f73a21240bdf9794
Jun <junj0619@gmail.com>
2017-07-28 15:14:44 -0400

    Create ExpressionAddOperators.java

---
+++ src/Homework8_ExpressionAddOperators_282/ExpressionAddOperators.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<>();
+        helper(res, "", num, target, 0, 0, 0);
+        return res;
+    }
+    
+    public void helper(List<String> res, String path, String num, int target, int pos, long preResult, long multied) {
+        if (pos == num.length()) {
+            if(preResult == target) {
+                res.add(path);
+            }
+            return;            
+        }
+
+        for (int i = pos; i < num.length(); i++) {
+            //["1*0+5", "1*5", "10-5"] ==> correct aws: ["1*0+5","10-5"]
+            //          105
+            //   1          10       105
+            // 0  05(skip)   5
+            //5
+            if(i != pos && num.charAt(pos) == '0') break;
+            long cur = Long.parseLong(num.substring(pos, i + 1));      //05 -> 5     
+            
+            if (pos == 0) {
+                helper(res, path + cur, num, target, i + 1, cur, cur);              
+            } else {
+                helper(res, path + "+" + cur, num, target, i + 1, preResult + cur,  cur);
+                helper(res, path + "-" + cur, num, target, i + 1, preResult - cur, -cur);
+                helper(res, path + "*" + cur, num, target, i + 1, (preResult - multied) + cur * multied , multied * cur);
+            }                                
+        }
+    }
+}
