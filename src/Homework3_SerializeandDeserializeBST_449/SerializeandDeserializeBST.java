94e9e7dcd2ffa3bf22589170115cf86dac8773fe
Jun <junj0619@gmail.com>
2017-09-28 23:04:26 -0400

    Update SerializeandDeserializeBST.java

--- src/Homework3_SerializeandDeserializeBST_449/SerializeandDeserializeBST.java
+++ src/Homework3_SerializeandDeserializeBST_449/SerializeandDeserializeBST.java
@@ -1,4 +1,70 @@
 package Homework3_SerializeandDeserializeBST_449;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        
+        StringBuilder sb = new StringBuilder();        
+        buildString(root, sb);
+        return sb.toString();
+    }
+    
+    public void buildString(TreeNode root, StringBuilder sb) {
+        if (root == null) return;
+        
+        sb.append(root.val + ",");
+        buildString(root.left, sb);
+        buildString(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == "") return null;
+        
+        String[] strs = data.split(",");
+        Queue<Integer> q = new LinkedList<>();
+        
+        for (String str : strs) {
+            q.offer(Integer.valueOf(str));                
+        }
+        
+        return buildTree(q);
+    }
+    
+    public TreeNode buildTree(Queue<Integer> queue) {
+        if (queue.isEmpty()) return null;
+        
+        TreeNode root = new TreeNode(queue.poll());
+        Queue<Integer> smallerQueue = new LinkedList<>();
+        
+        while (!queue.isEmpty() && root.val > queue.peek()) {
+            smallerQueue.offer(queue.poll());
+        }
+        
+        root.left = buildTree(smallerQueue);
+        root.right = buildTree(queue);
+        
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
+
+
+/**
+
 
 /**
  * Created by junjin on 6/22/17.
@@ -81,3 +147,4 @@ public class SerializeandDeserializeBST {
 // Codec codec = new Codec();
 // codec.deserialize(codec.serialize(root));
 }
+**/
