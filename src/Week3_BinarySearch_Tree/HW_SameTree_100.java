a8e454857f699627e87f525d5b443ab9d5b8f622
Jun <junj0619@gmail.com>
2017-10-11 22:42:45 -0400

    Update HW_SameTree_100.java

--- src/Week3_BinarySearch_Tree/HW_SameTree_100.java
+++ src/Week3_BinarySearch_Tree/HW_SameTree_100.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+//By Recursion
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {        
         if (p == null && q == null)
@@ -17,5 +18,33 @@ class Solution {
         
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);        
     }
+}
 
+//By BFS Level Traversal
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.offer(p);
+        queue.offer(q);
+        
+        while(!queue.isEmpty()) {
+            TreeNode nodeP = queue.poll();
+            TreeNode nodeQ = queue.poll();
+            
+            if (nodeP == null && nodeQ == null)
+                continue;
+            
+            if (nodeP == null || nodeQ == null || nodeP.val != nodeQ.val)
+                return false;
+            
+            queue.offer(nodeP.left);
+            queue.offer(nodeQ.left);
+            queue.offer(nodeP.right);
+            queue.offer(nodeQ.right);
+        }
+        return true;
+    }
 }
