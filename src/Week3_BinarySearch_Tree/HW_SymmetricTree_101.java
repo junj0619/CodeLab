e61ec70ceb126d78bef51222d75bdeeb70f2f02f
Jun <junj0619@gmail.com>
2017-10-11 21:59:29 -0400

    Update HW_SymmetricTree_101.java

--- src/Week3_BinarySearch_Tree/HW_SymmetricTree_101.java
+++ src/Week3_BinarySearch_Tree/HW_SymmetricTree_101.java
@@ -69,3 +69,21 @@ class Solution {
         return true;                
     }
 }
+
+//By Recursion
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return helper(root.left, root.right);
+    }
+    
+    public boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) 
+            return true;
+        
+        if (left == null || right == null || left.val != right.val)
+            return false;
+        
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+}
