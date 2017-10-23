ba8849da70f5b08aeca2033b6f5d609d0c00c1df
Jun <junj0619@gmail.com>
2017-10-22 23:08:27 -0400

    Create EX_BinaryTreeZigzagLevelOrderTraversal_103.java

---
+++ src/Week3_BinarySearch_Tree/EX_BinaryTreeZigzagLevelOrderTraversal_103.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ *
+ *      3
+ *     / \
+ *    9  20
+ *      /  \
+ *     15   7
+ * return its zigzag level order traversal as:
+ * [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ * ]
+ *
+ *
+ */
+ //By BFS Level Traversal
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+                
+        List<List<Integer>> resList = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        boolean isReverse = true;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            isReverse = !isReverse;
+                
+            for (int i = 0; i < size; i++) {                           
+                root = queue.poll();
+                if (isReverse) 
+                    list.add(0, root.val);
+                else
+                    list.add(root.val);
+                
+                if (root.left != null)
+                    queue.offer(root.left);
+                
+                if (root.right != null)
+                    queue.offer(root.right);
+            }                                    
+            resList.add(list);
+        }
+        
+        return resList;
+    }
+}
