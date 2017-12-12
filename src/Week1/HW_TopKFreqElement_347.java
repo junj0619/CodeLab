f43de9198586e810257b83359afa9257afcd8a9d
Jun <junj0619@gmail.com>
2017-12-12 00:29:48 -0500

    Update HW_TopKFreqElement_347.java

--- src/Week1/HW_TopKFreqElement_347.java
+++ src/Week1/HW_TopKFreqElement_347.java
@@ -24,4 +24,48 @@ class Solution {
     }
 }
 
-//Bucket Sort Pending to Learn
+//Bucket Sort: Time Complexity: O(n)
+//Use element frequence as Index then comb the result.
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (nums == null || nums.length == 0) return result;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();        
+       
+        //Conern case nums all same. eg:[1,1,1,1,1,1] k = 1 
+        //Then map is <1, 6>. in line of map.get(1) = 6 but bucket size is only 1
+        List<Integer>[] bucket = new List[nums.length + 1];
+            
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);            
+        }
+        
+        //Corner case nums all same. eg:[1,1,1,1,1,1] k = 1 
+        //Then map is <1, 6>. in line of map.get(1) = 6 but bucket size is only 1
+        //List<Integer>[] bucket = new List[map.size() + 1];
+        
+        for (int n : map.keySet()) {
+            int freq = map.get(n);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<Integer>();
+            }
+            bucket[freq].add(n);
+        }
+        
+        for (int i = bucket.length - 1; i > 0 && k > 0 ; i--) {
+            if (bucket[i] != null) {
+                result.addAll(bucket[i]);
+                // Corner cases two element has same freq
+                // e.g: [1, 1, 2, 2, 3, 4] , k = 2
+                // Expect: [1, 2]
+                // But if only use k--; 
+                // Wrong Result: [1, 2, 3, 4]
+                k = k - bucket[i].size();  
+            }
+        }
+        
+        return result;
+    }
+}
