db454fd460b05e622e3a5dd7f7f9d91a96a5bcd0
Jun <junj0619@gmail.com>
2017-12-23 23:32:49 -0500

    Add optimizated solution 1, 2

--- src/Week2_Pointers_LinearStructure/CLS_RansomNote_383.java
+++ src/Week2_Pointers_LinearStructure/CLS_RansomNote_383.java
@@ -22,4 +22,41 @@ class Solution {
         }
         return true;
     }
+    
+    //This is optimization solution 1
+    //Only use one array to save 26 letters.
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int magaLen = magazine.length();
+        int ransomLen = ransomNote.length();
+        int[] count = new int[26];
+        
+        for (int i = 0; i < magaLen; i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+        
+        for (int j = 0; j < ransomLen; j++) {            
+            if(count[ransomNote.charAt(j) - 'a'] == 0)
+                return false;
+            count[ransomNote.charAt(j) - 'a']--;
+        }
+        
+        return true;
+    }
+    //By CharArray Solution 2
+      public boolean canConstruct(String ransomNote, String magazine) { 
+        int[] count = new int[26];
+        
+        for (char maga : magazine.toCharArray()) {
+            count[maga - 'a']++;
+        }
+        
+        for (char ran : ransomNote.toCharArray()) {            
+            if(count[ran - 'a'] == 0)
+                return false;
+            count[ran - 'a']--;
+        }
+        
+        return true;
+    }
+  
 }
