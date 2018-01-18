8bbad8b3f132544a4e6701782949df17a9c25f16
Jun <junj0619@gmail.com>
2018-01-18 14:50:56 -0500

    Update HW_InsertInterval_57.java

--- src/Week2_Pointers_LinearStructure/HW_InsertInterval_57.java
+++ src/Week2_Pointers_LinearStructure/HW_InsertInterval_57.java
@@ -42,4 +42,67 @@ class Solution {
         
         return res;                                            
     }
+    
+    
+    //2) no sort insert interval base on overlap logic
+    
+     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {     
+        List<Interval> res = new ArrayList<Interval>();
+        if (intervals == null || intervals.size() == 0) {
+            res.add(newInterval);
+            return res;
+        }            
+        
+        //Step 3: merg intervals
+        int nStart = newInterval.start;
+        int nEnd = newInterval.end;
+        int start = 0;
+        int end = 0;  
+        
+        int len = intervals.size() + 1; //worst case interval has no place to merg
+        
+        int i = 0;
+        
+        while (i < len - 1) {            
+            int iStart = intervals.get(i).start;
+            int iEnd = intervals.get(i).end;
+                       
+            if (nStart > iEnd)  { //No overlap newInterval larger than intervals
+                res.add(intervals.get(i));       
+                start = nStart;
+                end = nEnd;
+                i++;               
+            } 
+            else if (nEnd < iStart) { //No overlap newInterval smaller than intervals
+                res.add(new Interval(nStart, nEnd));
+                start = iStart;
+                end = iEnd;
+                i++;  
+                break;
+            }
+            else if (nStart >= iStart || iEnd >= nStart) {  // Overlap                           
+                start = Math.min(iStart, nStart);                               
+                end = Math.max(iEnd, nEnd);               
+                break;
+            }            
+        }
+        
+        
+        while (i < len - 1) {    //Deal remaining intervals        
+            int iStart = intervals.get(i).start;
+            int iEnd = intervals.get(i).end;
+            
+            if (iStart > end) {
+                res.add(new Interval(start, end));
+                start = iStart;
+                end = iEnd;
+            } else 
+                end = Math.max(end, iEnd);
+            i++;
+        }
+        
+        res.add(new Interval(start, end));
+        
+        return res;                                            
+    }
 }
