d99f6e6ade3c3789913c3d0fb20408cb2b765bfb
Jun <junj0619@gmail.com>
2018-01-30 01:00:28 -0500

    Update OP_LongestSubStringWithAtMostTwoDistincit_159.java

--- src/Week2_Pointers_LinearStructure/OP_LongestSubStringWithAtMostTwoDistincit_159.java
+++ src/Week2_Pointers_LinearStructure/OP_LongestSubStringWithAtMostTwoDistincit_159.java
@@ -32,4 +32,51 @@ class Solution {
         }
         return ans;
     }
+    
+/* LC Approach: one pass siding windows O(n)
+* "The main idea is to maintain a sliding window with 2 unique characters. 
+*  The key is to store the last occurrence of each character as the value in the hashmap. 
+*  This way, whenever the size of the hashmap exceeds 2, 
+*  we can traverse through the map to find the character with the left most index, 
+*  and remove 1 character from our map. Since the range of characters is constrained, 
+*  we should be able to find the left most index in constant time."
+* 
+* e c e b a
+* []           [e, 0] max = 1 - 0 = 1
+* [ ]          [e, 0], [c, 1] max = 2 - 0 = 2
+* [   ]        [e, 2], [c, 1] max = 3 - 0 = 3
+* [     ]      [e, 2], [c, 1], [b, 3]  => [e, 2], [b, 3]  leftMost = 1, lo = 1 + 1 = 2
+*     [ ]      ,max = 4 - 2 = 2
+*     [    ]   [e, 2], [b, 3], [a, 4] => [b, 3], [a, 4] leftMost = 2, lo = 2 + 1 = 3
+*	    [  ]   ,max = 5 - 3 = 2
+*
+*/               
+
+
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        if (s == null || s.length() < 1) return 0;
+        
+        HashMap<Character, Integer> map = new HashMap<>(); //Key: char, Value: most right index;
+        int hi = 0, low = 0, maxLen = 0, len = s.length();
+        char[] chars = s.toCharArray();
+        
+        while (hi < len) {
+            if (map.size() <= 2) {
+                map.put(chars[hi], hi);
+                hi++;
+            }
+            
+            if (map.size() > 2) {
+                int leftMost = len;
+                for (int idx : map.values()) 
+                    leftMost = Math.min(leftMost, idx);                
+                map.remove(chars[leftMost]);           
+                low = leftMost + 1;
+            }           
+            
+            maxLen = Math.max(hi - low, maxLen);
+        }
+        return maxLen;        
+    }
+    
 }
