c1678d3bb63ccf8e73dd0ce766b4da25f22f8fb1
Jun <junj0619@gmail.com>
2018-01-31 10:03:05 -0500

    Update EX_LongestSubStringWithOutRepeat.java

--- src/Week2_Pointers_LinearStructure/EX_LongestSubStringWithOutRepeat.java
+++ src/Week2_Pointers_LinearStructure/EX_LongestSubStringWithOutRepeat.java
@@ -1,5 +1,5 @@
 class Solution {
-//By Silding Window
+//Approach #1: By Silding Window O(2n)
     public int lengthOfLongestSubstring(String s) {
         if (s == null) return 0;
         
@@ -17,4 +17,27 @@ class Solution {
         }        
         return ans;        
     }
+    
+////Approach #2: By Silding Window O(n) 
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        
+        int lo = 0, hi = 0, len = s.length(), res = 0;
+        char[] chars = s.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>(); //Key: char, Value: index        
+        
+        while (hi < len) {
+            if (map.containsKey(chars[hi])) {                
+                lo = Math.max(lo, map.get(chars[hi]));   //Need max because some case like "abba"                        
+            } 
+            
+            map.put(chars[hi], hi + 1);  //to avoid low equals map.index and get next start index without mannual increase                              
+            res = Math.max(res, hi - lo + 1);
+            hi++;
+        }
+        
+        return res;
+    }
 }
+
+
