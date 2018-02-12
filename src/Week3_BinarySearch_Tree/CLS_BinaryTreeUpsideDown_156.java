9895acc8ea06584345da47dd84978c3a58383edc
Jun <junj0619@gmail.com>
2018-02-11 23:13:16 -0500

    Update CLS_BinaryTreeUpsideDown_156.java

--- src/Week3_BinarySearch_Tree/CLS_BinaryTreeUpsideDown_156.java
+++ src/Week3_BinarySearch_Tree/CLS_BinaryTreeUpsideDown_156.java
@@ -18,10 +18,13 @@
  * 5   2
  *    / \
  *   3   1  
+ */
+
 class Solution {
-    public TreeNode upsideDownBinaryTree(TreeNode root) {
-        //Time Complexity: O(n)
-        //Space Complexity: O(n/2) = O(n)  because input tree only have two branchs and we only need do level of tree recursion stacks
+    //Approach 1:
+    //Time Complexity: O(n)
+    //Space Complexity: O(n/2) = O(n)  because input tree only have two branchs and we only need do level of tree recursion stacks
+    public TreeNode upsideDownBinaryTree(TreeNode root) {        
         if (root == null || root.left == null) 
             return root;
         
@@ -32,5 +35,24 @@ class Solution {
         root.right = null;
         
         return newRoot;                
+    }
+    //Approach 2:
+    //Time Complexity: O(n), Space Compleixty: O(1) only use 3 pointers
+     public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null) return null;
+        
+        TreeNode sib = null, par = null, next = null;                           
+        
+        while (root != null) {
+            next = root.left;
+            
+            root.left = sib;
+            sib = root.right;
+            root.right = par;            
+           
+            par = root;
+            root = next;            
+        }
+        return par;
     }
 }
