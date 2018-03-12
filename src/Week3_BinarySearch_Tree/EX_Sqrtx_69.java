faa9e9885fe5d3275d73ee8f6a84e8cd7e7d36d2
Jun <junj0619@gmail.com>
2018-03-11 23:22:51 -0400

    Update EX_Sqrtx_69.java

--- src/Week3_BinarySearch_Tree/EX_Sqrtx_69.java
+++ src/Week3_BinarySearch_Tree/EX_Sqrtx_69.java
@@ -1,19 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-       if (x == 0) return 0;
-        
-        int start = 1;
-        int end = x;
+        if (x <= 0) return 0;
+        long start = 0, end = x;
         
         while (start < end - 1) {
-            int mid = start + (end - start) / 2;
-            if (mid == x / mid)
-                return mid;
-            else if (mid < x / mid)
-                start = mid;
-            else
+            long mid = start + (end - start) / 2;
+            
+            if (mid * mid == x)
+                return (int) mid;
+            else if (mid * mid > x)
                 end = mid;
+            else
+                start = mid;
         }
-        return start;
+        //26, 25
+        if (end * end <= x)
+            return (int) end;
+        else 
+            return (int) start;
     }
 }
