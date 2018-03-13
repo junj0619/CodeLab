bf93d6819a38de9a255faf327f9ae5cde9008b73
Jun <junj0619@gmail.com>
2018-03-13 17:00:21 -0400

    Update BubbleSort.java

--- src/Week4_SortAndGraph/BubbleSort.java
+++ src/Week4_SortAndGraph/BubbleSort.java
@@ -23,25 +23,49 @@
 * ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
 * ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
 *
+* Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+* Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+*
+*
 **/
 
-// Java program for implementation of Bubble Sort
-class BubbleSort
-{
-  public void bubbleSort(int arr[])
-	{
-	    int n = arr.length;
-	    
-	    for (int i = 0; i < n; i++){
-	        for (int j = 0; j < n - i - 1; j++) {
-	            if (arr[j + 1] < arr[j]) { //then swap adjacent elements
-	                int temp = arr[j + 1];
-	                arr[j + 1] = arr[j];
-	                arr[j] = temp;
-	            }
-	        }
-	    }
-	}
+class BubbleSort {
+    public void bubbleSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j + 1] < arr[j]) { //then swap adjacent elements
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+/*
+ * Optimized Implementation:
+ * The above function always runs O(n^2) time even if the array is sorted.  
+ * It can be optimized by stopping the algorithm if inner loop didn’t cause any swap.	
+ **/
+    public void bubbleSort2(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j + 1] < arr[j]) { //then swap adjacent elements
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
 
 }
 
