74481cc59ef24123329cd6487123d770f355b401
Jun <junj0619@gmail.com>
2018-03-13 17:01:16 -0400

    Update BubbleSort.java

--- src/Week4_SortAndGraph/BubbleSort.java
+++ src/Week4_SortAndGraph/BubbleSort.java
@@ -25,7 +25,7 @@
 *
 * Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
 * Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
-*
+* Stable: Yes
 *
 **/
 
