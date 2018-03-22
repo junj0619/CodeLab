4e62e0d0194ab1bf448883d4cf0f7fb05b2020ff
Jun <junj0619@gmail.com>
2018-03-22 14:05:16 -0400

    Create 3.1-ShellSort.java

---
+++ src/Week4_SortAndGraph/3.1-ShellSort.java
@@ -0,0 +1,28 @@
+/* Java implementation of ShellSort
+*  https://www.geeksforgeeks.org/shellsort/
+*  
+* Time Complexity: Time complexity of above implementation of shellsort is O(n2). 
+* In the above implementation gap is reduce by half in every iteration. 
+* There are many other ways to reduce gap which lead to better time complexity. 
+*
+*/
+
+class ShellSort
+{
+  public void sort(int[] nums) {
+        int len = nums.length;
+        // Start with a big gap, then reduce the gap
+        for (int gap = len / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < len; i++) {
+                int temp = nums[i];
+                int pos;
+                for (pos = i; pos >= gap && nums[pos - gap] > temp; pos -= gap) 
+                    nums[pos] = nums[pos - gap];
+                    
+                // put temp (the original a[i]) in its correct
+                nums[pos] = temp;
+            }
+        }
+    }
+    
+}    
