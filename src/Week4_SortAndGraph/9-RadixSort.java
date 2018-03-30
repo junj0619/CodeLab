2883b9ce7f0beb5c14505442ad2d7f1712873e78
Jun <junj0619@gmail.com>
2018-03-29 23:25:34 -0400

    Update 9-RadixSort.java

--- src/Week4_SortAndGraph/9-RadixSort.java
+++ src/Week4_SortAndGraph/9-RadixSort.java
@@ -1,7 +1,12 @@
 public class RadixSort {
-
+/*
+*   Time Complexity: O(nd)  => n is length of input, d is max num length 
+*   Space Complexity: O(nd) 
+*   Stable: Y (must stable to make RadixSort work)  => eg. 123, 121, 124
+*
+*/
 	public int[] sort(int[] nums) {
-		for (int i = 1; i < 1000000000; i *= 10) {
+		for (int i = 1; i < 1000000000; i *= 10) { //Largest place for a 32-bit int is 1's billions space. 
 			nums = countingSort(nums, i);
 		}
 		return nums;
