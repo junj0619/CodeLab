53ab0668a6ac2656e2b62ee9671e9f2fc2ce82e7
Jun <junj0619@gmail.com>
2018-04-18 12:55:28 -0400

    Create CLS1_98.ValidateBinarySearch Tree.java

---
+++ src/CS1802/Week2/CLS1_98.ValidateBinarySearch Tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+   //#1 By Iteration
+    public boolean isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode pre = null;        
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            
+            TreeNode node = stack.pop();
+            if (pre != null && node.val <= pre.val) return false;
+            pre = node;
+            root = node.right;
+        }
+        return true;
+    }
+    
+    
+    //#2 By Recursion
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val <= min || root.val >= max) return false;
+        
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+}
