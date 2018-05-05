c62f94ab2435023b8d58694a96ad5e9ccd0d16b7
Jun <junj0619@gmail.com>
2018-05-04 23:39:59 -0400

    Update CLS2_139.Word Break.java

--- src/CS1802/Week4/CLS2_139.Word Break.java
+++ src/CS1802/Week4/CLS2_139.Word Break.java
@@ -4,6 +4,7 @@
 * dp[0] means empty string
 * dp[i] = true means from 0 to i positon is compostion of wordDict. Otherwise not a compostion.
 * Time Complexity: O(n ^ 2) we calculate each position dp value and try any [0 - j) && [j - i) combinations in iteration.
+*                  If we know [0 - j) is composition of wordDict then just need to check [j - i) is in the wordDict
 */
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
